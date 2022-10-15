b837b942c4876396495ecbe6abfd6907a389e69d
ctonton <51173996+ctonton@users.noreply.github.com>
2022-10-15 17:36:18 -0500

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -259,11 +259,11 @@ systemctl start qbittorrent
 echo
 echo "Setting up Firefox."
 mkdir /root/.vnc
-tee /root/.vnc/config > /dev/null <<EOT
-session=jwm
-localhost=no
-SecurityTypes=None
+tee /root/.vnc/xstartup > /dev/null <<EOT
+#!/bin/bash
+/usr/bin/jwm
 EOT
+chmod +x /root/.vnc/xstartvnc
 tee /root/.jwmrc > /dev/null <<EOT
 <?xml version="1.0"?>
 <JWM>
@@ -343,7 +343,7 @@ Description=Remote desktop service (VNC)
 After=network.target
 [Service]
 Type=forking
-ExecStart=/usr/bin/tigervncserver --I-KNOW-THIS-IS-INSECURE :0
+ExecStart=/usr/bin/tigervncserver -localhost no -SecurityTypes None --I-KNOW-THIS-IS-INSECURE :0
 ExecStop=/usr/bin/tigervncserver -kill :0
 [Install]
 WantedBy=multi-user.target
