f4f4aeb46f2dc720d8c0e3908266af86627048e2
ctonton <51173996+ctonton@users.noreply.github.com>
2022-10-19 16:31:58 -0500

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -37,7 +37,7 @@ echo "0 4 * * 1 reboot" > tempcron
 crontab tempcron
 rm tempcron
 cp $0 /root/resume.sh
-sed -i '2,42d' /root/resume.sh
+sed -i '2,47d' /root/resume.sh
 chmod +x /root/resume.sh
 echo "bash /root/resume.sh" > /root/.bash_profile
 chmod +x /root/.bash_profile
