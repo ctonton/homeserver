52ff4ed466c997e4bfe17d4a621193681737ffdd
ctonton <51173996+ctonton@users.noreply.github.com>
2022-10-21 10:18:32 -0500

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -67,7 +67,7 @@ read -p "Enter disk partition (ex. sda2): " part
 uniq=$(blkid -o value -s UUID /dev/${part})
 type=$(blkid -o value -s TYPE /dev/${part})
 tee -a /etc/fstab > /dev/null <<EOT
-UUID=${uniq}  /srv/NAS  ${type}  defaults,nofail  0  0
+UUID=${uniq}  /srv/NAS  ${type}  defaults,x-systemd.before=nfs-kernel-server.service,nofail  0  0
 EOT
 mount -a
 
