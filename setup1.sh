630558573440743cab8f2fff3a4a6914215c83f5
ctonton <51173996+ctonton@users.noreply.github.com>
2023-03-22 16:32:40 -0500

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -33,12 +33,13 @@ sed -i "s/$HOSTNAME/$serv/g" /etc/hosts
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
 apt-get update
-apt-get install -y --no-install-recommends openssh-server
 apt-get full-upgrade -y --fix-missing
+apt-get install -y --no-install-recommends openssh-server
 sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+systemctl enable ssh
 echo "0 4 * * 1 /sbin/reboot" | crontab -
 cp $0 /root/resume.sh
-sed -i '2,49d' /root/resume.sh
+sed -i '2,50d' /root/resume.sh
 chmod +x /root/resume.sh
 echo "bash /root/resume.sh" > /root/.bash_profile
 chmod +x /root/.bash_profile
