2f431a8fa35237f285347b81638bca523d569523
ctonton <51173996+ctonton@users.noreply.github.com>
2023-04-08 16:31:32 -0500

    Update cups.sh

--- individual_scripts/cups.sh
+++ individual_scripts/cups.sh
@@ -2,13 +2,9 @@
 echo "Setting up CUPS."
 usermod -aG lpadmin root
 cupsctl --remote-admin --user-cancel-any
-read -p "Do you want to set up the default printer now? (y/n): " cont
-if [ $cont == "y" ]
-then
-  read -p "Enter the static IP address of the default printer: $(/sbin/ip route | awk '/src/ { print $1 }' | cut -f1-3 -d".")." prip
-  defip=$(/sbin/ip route | awk '/src/ { print $1 }' | cut -f1-3 -d".").${prip}
-  read -p "Enter a name for the default printer: " defpr
-  lpadmin -p $defpr -E -v ipp://${defip}/ipp/print -m everywhere
-  lpadmin -d $defpr
-fi
+read -p "Enter the static IP address of the default printer: $(/sbin/ip route | awk '/src/ { print $1 }' | cut -f1-3 -d".")." prip
+defip=$(/sbin/ip route | awk '/src/ { print $1 }' | cut -f1-3 -d".").${prip}
+read -p "Enter a name for the default printer: " defpr
+lpadmin -p $defpr -E -v ipp://${defip}/ipp/print -m everywhere
+lpadmin -d $defpr
 exit
