5ad1ead888e05fb2958049b36f0b95f8207dcb36
ctonton <51173996+ctonton@users.noreply.github.com>
2023-04-08 13:55:31 -0500

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -215,52 +215,34 @@ fi
 #ddns
 echo
 echo "Installing DuckDNS."
-mkdir /root/.ddns
-tee /root/.ddns/duck.sh > /dev/null <<'EOT'
+tee /root/setup-ddns.sh > /dev/null <<'EOT'
 #!/bin/bash
-domain=enter_domain
+tee /etc/NetworkManager/dispatcher.d/99-ddns  > /dev/null <<'EOT'
+#!/bin/sh
 token=enter_token
-ipv6addr=$(curl -s https://api6.ipify.org)
-ipv4addr=$(curl -s https://api.ipify.org)
-curl -s "https://www.duckdns.org/update?domains=$domain&token=$token&ip=$ipv4addr&ipv6=$ipv6addr"
+domain=enter_domain
+if [ "$2" = "up" ]
+then
+  sleep 15
+  ipv6addr=$(curl -s https://api6.ipify.org)
+  ipv4addr=$(curl -s https://api.ipify.org)
+  curl -s "https://www.duckdns.org/update?domains=$domain&token=$token&ip=$ipv4addr&ipv6=$ipv6addr"
+fi
+exit 0
 EOT
-chmod +x /root/.ddns/duck.sh
-tee /etc/systemd/system/ddns.service > /dev/null <<'EOT'
-[Unit]
-Description=DynDNS Updater services
-After=network-online.target
-[Service]
-Type=simple
-ExecStartPre=/bin/sleep 30
-ExecStart=/root/.ddns/duck.sh
-TimeoutSec=60
-[Install]
-WantedBy=network-online.target
+chmod +x /etc/NetworkManager/dispatcher.d/99-ddns  
+read -p "Enter the token from duckdns.org: " token
+sed -i "s/enter_token/$token/g" /etc/NetworkManager/dispatcher.d/99-ddns
+read -p "Enter the domain from duckdns.org: " domain
+sed -i "s/enter_domain/$domain/g" /etc/NetworkManager/dispatcher.d/99-ddns
+cat <(crontab -l) <(echo "0 */2 * * * /etc/NetworkManager/dispatcher.d/99-ddns") | crontab -
+rm $0
+return
 EOT
 read -p "Do you want to set up Dynamic DNS now? (y/n): " cont
 if [ $cont == "y" ]
 then
-  read -p "Enter the token from duckdns.org: " token
-  sed -i "s/enter_token/$token/g" /root/.ddns/duck.sh
-  read -p "Enter the domain from duckdns.org: " domain
-  sed -i "s/enter_domain/$domain/g" /root/.ddns/duck.sh
-  systemctl enable ddns
-  cat <(crontab -l) <(echo "0 1 * * * /root/.ddns/duck.sh") | crontab -
-else
-  tee /root/setup-ddns.sh > /dev/null <<'EOT'
-#!/bin/bash
-clear
-read -p "Enter the token from duckdns.org: " token
-sed -i "s/enter_token/$token/g" /root/.ddns/duck.sh
-read -p "Enter the domain from duckdns.org: " domain
-sed -i "s/enter_domain/$domain/g" /root/.ddns/duck.sh
-systemctl enable ddns
-systemctl start ddns
-cat <(crontab -l) <(echo "0 1 * * * /root/.ddns/duck.sh") | crontab -
-rm $0
-exit
-EOT
-  chmod +x /root/setup-ddns.sh  
+bash /root/setup-ddns.sh
 fi
 
 #qbittorrent
