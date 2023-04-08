5cfe6dcd12092a329d5b09b988b912a6d7499cbe
ctonton <51173996+ctonton@users.noreply.github.com>
2023-04-08 17:21:37 -0500

    Create wireguard.sh

---
+++ individual_scripts/wireguard.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Downloading WireGuard setup script to the root directory."
+curl -LJ https://github.com/Nyr/wireguard-install/raw/master/wireguard-install.sh -o /root/setup-wireguard.sh
+chmod +x /root/setup-wireguard.sh
+bash /root/setup-wireguard.sh
+ufw allow from 10.7.0.0/24
+ufw allow 51820/udp
+sed -i '/forward=1/s/^# *//' /etc/sysctl.conf
+sed -i '/forwarding=1/s/^# *//' /etc/sysctl.conf
+sed -i '/^WebUI\\AuthSubnetWhitelist=/ s/$/,10.7.0.0\/24/' /root/.config/qBittorrent/qBittorrent.conf
+exit
