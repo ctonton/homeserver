a4e91efb0ad8a4f952263c3e70df3ecf0fa2fe1c
ctonton <51173996+ctonton@users.noreply.github.com>
2023-04-08 16:45:59 -0500

    Update samba.sh

--- individual_scripts/samba.sh
+++ individual_scripts/samba.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Setting up SAMBA."
+apt-get install -y --no-install-recommends samba
 if [ ! -f /etc/samba/smb.bak ]
 then
   mv /etc/samba/smb.conf /etc/samba/smb.bak
@@ -20,9 +21,10 @@ tee /etc/samba/smb.conf > /dev/null <<EOT
    create mask = 0777
    directory mask = 0777
 EOT
+systemctl restart smbd
 curl -LJ https://raw.githubusercontent.com/christgau/wsdd/master/src/wsdd.py -o /usr/bin/wsdd
 chmod +x /usr/bin/wsdd
-tee /etc/systemd/system/wsdd.service > /dev/null <<EOT
+tee /etc/systemd/system/wsdd.service > /dev/null <<'EOT'
 [Unit]
 Description=Web Services Dynamic Discovery host daemon
 After=network-online.target
@@ -34,4 +36,5 @@ ExecStart=/usr/bin/wsdd -s -4
 WantedBy=multi-user.target
 EOT
 systemctl enable wsdd
+systemctl start wsdd
 exit
