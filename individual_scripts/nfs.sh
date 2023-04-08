4c1afdeb93ade78c074b83ef1554b0fe3817ecd7
ctonton <51173996+ctonton@users.noreply.github.com>
2023-04-08 16:54:09 -0500

    Update nfs.sh

--- individual_scripts/nfs.sh
+++ individual_scripts/nfs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Setting up NFS."
+apt-get install -y --no-install-recommends nfs-kernel-server avahi-daemon
 tee -a /etc/exports > /dev/null <<EOT
 /srv/NAS/Public *(rw,sync,all_squash,no_subtree_check)
 EOT
