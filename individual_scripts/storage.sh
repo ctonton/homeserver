e4b9c3082b52f3d28b94b5ac65d009ded8392cb7
ctonton <51173996+ctonton@users.noreply.github.com>
2023-04-18 17:19:54 -0500

    Update storage.sh

--- individual_scripts/storage.sh
+++ individual_scripts/storage.sh
@@ -5,11 +5,15 @@ chmod 777 /srv/NAS
 chown nobody:nogroup /srv/NAS
 blkid
 echo
-read -p "Enter disk partition (ex. sda2): " part
-uniq=$(blkid -o value -s UUID /dev/${part})
-type=$(blkid -o value -s TYPE /dev/${part})
-tee -a /etc/fstab > /dev/null <<EOT
-UUID=${uniq}  /srv/NAS  ${type}  defaults,x-systemd.before=nfs-kernel-server.service,nofail  0  0
-EOT
-mount -a
+read -p "Disk partition to mount (ex. sda2): " part
+if [ -b /dev/$part ] && ! grep -q /dev/$part /proc/mounts
+then
+  uniq=$(blkid -o value -s UUID /dev/${part})
+  type=$(blkid -o value -s TYPE /dev/${part})
+  echo "UUID=${uniq}  /srv/NAS  ${type}  defaults,x-systemd.before=nfs-kernel-server.service,nofail  0  0" >> /etc/fstab
+  mount -a
+else
+  echo "#UUID=  /srv/NAS    defaults,x-systemd.before=nfs-kernel-server.service,nofail  0  0" >> /etc/fstab
+  read -n 1 -s -r -p "Device is not available. Press any key to continue without mounting storage."
+fi
 exit
