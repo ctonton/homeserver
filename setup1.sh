56f731d79f0682c7f3f9ece9817e393bf013f8a0
ctonton <51173996+ctonton@users.noreply.github.com>
2023-04-18 13:52:31 -0500

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -18,7 +18,7 @@ then
   read -n 1 -s -r -p "Network is not online. Press any key to exit."
   exit
 fi
-echo "This server needs a static IP addresses on the local network."
+echo "This server needs to have a static IP addresses on the local network."
 echo "An account at ngrok.com and authtoken are required to setup remote access to this server."
 read -p "Are you ready to proceed with the installation? (y/n): " cont
 if [ $cont != "y" ]
@@ -53,7 +53,7 @@ rm /root/.bash_profile
 #install
 clear
 echo "Installing software."
-apt-get install -y --no-install-recommends ntfs-3g curl tar unzip nfs-kernel-server samba avahi-daemon qbittorrent-nox
+apt-get install -y --no-install-recommends ntfs-3g curl tar unzip gzip nfs-kernel-server samba avahi-daemon qbittorrent-nox nginx openssl
 
 #storage
 clear
@@ -116,43 +116,96 @@ WantedBy=multi-user.target
 EOT
 systemctl enable wsdd
 
-#http
+#ngrok
 echo
-echo "Setting up http server"
-tee /etc/systemd/system/pyhttp.service > /dev/null <<'EOT'
+echo "Installing ngrok."
+if [ $(dpkg --print-architecture) = "armhf" ]
+then
+  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.tgz -o ngrok.tgz
+elif [ $(dpkg --print-architecture) = "i386" ]
+then
+  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.tgz -o ngrok.tgz
+elif [ $(dpkg --print-architecture) = "arm64" ]
+then
+  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.tgz -o ngrok.tgz
+elif [ $(dpkg --print-architecture) = "amd64" ]
+then
+  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz -o ngrok.tgz
+fi
+tar xvf ngrok.tgz -C /usr/local/bin
+rm ngrok.tgz
+mkdir /root/.ngrok2
+tee /root/.ngrok2/ngrok.yml > /dev/null <<EOT
+authtoken: noauth
+tunnels:
+  nginx:
+    addr: 443
+    proto: http
+    bind_tls: true
+    inspect: false
+  ssh:
+    addr: 22
+    proto: tcp
+    inspect: false
+EOT
+tee /etc/systemd/system/ngrok.service > /dev/null <<'EOT'
 [Unit]
-Description=python http server
+Description=ngrok
 After=network.target
 [Service]
 Type=exec
-ExecStart=/usr/bin/python3 -m http.server -d /srv/NAS 8000
-Restart=on-failure
+ExecStart=/usr/local/bin/ngrok start --all
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=process
+IgnoreSIGPIPE=true
+Restart=always
+RestartSec=3
 [Install]
 WantedBy=multi-user.target
 EOT
-systemctl enable pyhttp
+read -p "Do you want to set up access to this server through ngrok? y/n: " cont
+if [ $cont == "y" ]
+then
+  read -p "Enter your ngrok Authtoken: " auth
+  sed -i "s/noauth/$auth/g" /root/.ngrok2/ngrok.yml
+  systemctl enable ngrok
+else
+  tee /root/setup-ngrok.sh > /dev/null <<'EOT'
+#!/bin/bash
+clear
+read -p "Enter your ngrok Authtoken: " auth
+sed -i "s/noauth/$auth/g" /root/.ngrok2/ngrok.yml
+systemctl enable ngrok
+systemctl start ngrok
+rm $0
+exit
+EOT
+  chmod +x /root/setup-ngrok.sh
+fi
 
 #qbittorrent
 echo
 echo "Setting up qBittorrent."
+echo
+echo "*** Legal Notice ***"
+echo "qBittorrent is a file sharing program. When you run a torrent, its data will be made available to others by means of upload. Any content you share is your sole responsibility."
+echo
+echo"No further notices will be issued."
+echo
+read -n 1 -s -r -p "Press any key to accept and continue..."
 mkdir -p /root/.config/qBittorrent
-curl -LJO https://github.com/ctonton/homeserver/raw/main/blocklist.zip
-unzip -o blocklist.zip -d /root/.config/qBittorrent
-rm blocklist.zip
+curl -LJ https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz -o /root/.config/qBittorrent/blocklist.p2p.gz
+gzip -d /root/.config/qBittorrent/blocklist.p2p.gz
 tee /root/.config/qBittorrent/qBittorrent.conf > /dev/null <<EOT
 [AutoRun]
 enabled=true
 program=chown -R nobody:nogroup \"%R\"
-
 [BitTorrent]
 Session\GlobalMaxSeedingMinutes=1
-
 [LegalNotice]
 Accepted=true
-
 [Network]
 Cookies=@Invalid()
-
 [Preferences]
 Bittorrent\MaxRatioAction=1
 Connection\GlobalUPLimit=10
@@ -185,81 +238,114 @@ ExecStart=/usr/bin/qbittorrent-nox -d
 WantedBy=multi-user.target
 EOT
 systemctl enable qbittorrent
-
-#ngrok
-echo
-echo "Installing ngrok."
-if [ $(dpkg --print-architecture) = "armhf" ]
-then
-  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.tgz -o ngrok.tgz
-elif [ $(dpkg --print-architecture) = "i386" ]
-then
-  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.tgz -o ngrok.tgz
-elif [ $(dpkg --print-architecture) = "arm64" ]
-then
-  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.tgz -o ngrok.tgz
-elif [ $(dpkg --print-architecture) = "amd64" ]
-then
-  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz -o ngrok.tgz
-fi
-tar xvf ngrok.tgz -C /usr/local/bin
-rm ngrok.tgz
-mkdir /root/.ngrok2
-read -p "Do you want to set up access to this server through ngrok? y/n: " cont
-if [ $cont == "y" ]
-then
-  read -p "Enter your ngrok Authtoken: " auth
-else
-  auth=none
-  tee /root/setup-ngrok.sh > /dev/null <<'EOT'
+systemctl start qbittorrent
+tee /root/.config/qBittorrent/updatelist.sh > /dev/null <<EOT
 #!/bin/bash
-clear
-read -p "Enter your ngrok Authtoken: " auth
-sed -i "s/none/$auth/g" /root/.ngrok2/ngrok.yml
-systemctl enable ngrok
-systemctl start ngrok
-rm $0
+curl -LJ https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz -o /root/.config/qBittorrent/blocklist.p2p.gz
+gzip -df /root/.config/qBittorrent/blocklist.p2p.gz
+systemctl restart qbittorrent
 exit
 EOT
-  chmod +x /root/setup-ngrok.sh
-fi
-tee /root/.ngrok2/ngrok.yml > /dev/null <<EOT
-authtoken: ${auth}
-tunnels:
-  pyhttp:
-    addr: 8000
-    proto: http
-    bind_tls: true
-    inspect: false
-  qbittorrent:
-    addr: 8080
-    proto: http
-    bind_tls: true
-    inspect: false
-  ssh:
-    addr: 22
-    proto: tcp
-    inspect: false
-EOT
-tee /etc/systemd/system/ngrok.service > /dev/null <<'EOT'
-[Unit]
-Description=ngrok
-After=network.target
-[Service]
-ExecStart=/usr/local/bin/ngrok start --all
-ExecReload=/bin/kill -HUP $MAINPID
-KillMode=process
-IgnoreSIGPIPE=true
-Restart=always
-RestartSec=3
-Type=exec
-[Install]
-WantedBy=multi-user.target
-EOT
-if [ $auth != "none" ]
+chmod +x /root/.config/qBittorrent/updatelist.sh
+cat <(crontab -l) <(echo "30 4 * * 1 /root/.config/qBittorrent/updatelist.sh") | crontab -
+
+#nginx
+echo
+echo "Setting up NGINX."
+if [ ! -f /var/www/html/index.bak ]
 then
-  systemctl enable ngrok
+  mv /var/www/html/index* /var/www/html/index.bak
 fi
+curl -LJO https://github.com/ctonton/homeserver/raw/main/icons.zip
+unzip -o icons.zip -d /var/www/html
+rm icons.zip
+ln -s /srv/NAS/Public /var/www/html/files
+mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+tee /var/www/html/index.html > /dev/null <<'EOT'
+<html>
+  <head>
+    <title>Server</title>
+	  <style></style>
+  </head>
+  <body style="background-color:#000000;color:yellow;font-size:125%;text-align:center">
+    <h1>File Server</h1>
+    <a href="/files/"><img src="fs.png" alt="HTTP Server"></a>
+    <br>
+    <br>
+    <h1>Torrent Server</h1>
+    <a href="/torrents/"><img src="qb.png" alt="Qbittorrent"></a>
+    <br>
+    <br>
+  </body>
+</html>
+EOT
+chown -R www-data:www-data /var/www/html
+tee /etc/nginx/sites-available/default > /dev/null <<'EOT'
+##
+map $http_upgrade $connection_upgrade {
+	default upgrade;
+	''      close;
+}
+##
+server {
+listen 80 default_server;
+listen [::]:80 default_server;
+	location / {
+	return 301 https://$host$request_uri;
+	}
+}
+##
+server {
+	listen 443 ssl http2;
+	listen [::]:443 ssl http2;
+	ssl_certificate /etc/nginx/nginx-selfsigned.crt;
+	ssl_certificate_key /etc/nginx/nginx-selfsigned.key;
+	ssl_session_timeout  10m;
+	ssl_session_cache shared:SSL:10m;
+	ssl_session_tickets off;
+	ssl_dhparam /etc/nginx/dhparam.pem;
+	ssl_protocols TLSv1.2 TLSv1.3;
+	ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+	ssl_prefer_server_ciphers off;
+	resolver 8.8.8.8 8.8.4.4 valid=300s;
+	resolver_timeout 5s;
+	add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
+	add_header X-Frame-Options DENY;
+	add_header X-Content-Type-Options nosniff;
+	add_header X-XSS-Protection "1; mode=block";
+	client_max_body_size 10M;
+	root /var/www/html;
+	index index.html;
+	autoindex on;
+	location /files/ {
+		try_files $uri $uri/ =404;
+		auth_basic "Restricted Content";
+		auth_basic_user_file /etc/nginx/.htpasswd;
+	}
+	location /torrents/ {
+		proxy_pass http://127.0.0.1:8080/;
+		proxy_buffering off;
+		auth_basic "Restricted Content";
+		auth_basic_user_file /etc/nginx/.htpasswd;
+	}
+}
+EOT
+sed -i 's/www-data/root/g' /etc/nginx/nginx.conf
+curl -LJ https://raw.githubusercontent.com/ctonton/homeserver/main/individual_scripts/webusers.sh -o /root/webusers.sh
+chmod +x /root/webusers.sh
+echo
+echo "A script called webusers.sh has been added the root directory for modifying users of the web server."
+echo "Add a user for the web server now."
+loo="y"
+until [ $loo != "y" ]
+do
+  read -p "Enter a user name: " use
+  echo -n "${use}:" >> /etc/nginx/.htpasswd
+  openssl passwd -apr1 >> /etc/nginx/.htpasswd
+  read -p "Add another user? (y/n): " loo
+done
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/nginx/nginx-selfsigned.key -out /etc/nginx/nginx-selfsigned.crt
+curl https://ssl-config.mozilla.org/ffdhe4096.txt > /etc/nginx/dhparam.pem
 
 #cleanup
 apt-get autoremove
