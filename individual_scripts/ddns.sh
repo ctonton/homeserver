8dd133a11e7fc6ef6c7ffc0986e754faa554a778
ctonton <51173996+ctonton@users.noreply.github.com>
2023-04-18 19:06:40 -0500

    Update ddns.sh

--- individual_scripts/ddns.sh
+++ individual_scripts/ddns.sh
@@ -1,23 +1,34 @@
-#!/bin/bash
+#ddns
+echo
 echo "Installing DuckDNS."
-apt-get install -y --no-install-recommends curl
-tee /etc/NetworkManager/dispatcher.d/99-ddns  > /dev/null <<'EOT'
-#!/bin/sh
-token=enter_token
+mkdir /root/.ddns
+tee /root/.ddns/duck.sh > /dev/null <<'EOT'
+#!/bin/bash
 domain=enter_domain
-if [ "$2" = "up" ]
-then
-  sleep 15
-  ipv6addr=$(curl -s https://api6.ipify.org)
-  ipv4addr=$(curl -s https://api.ipify.org)
-  curl -s "https://www.duckdns.org/update?domains=$domain&token=$token&ip=$ipv4addr&ipv6=$ipv6addr"
-fi
-exit 0
+token=enter_token
+ipv6addr=$(curl -s https://api6.ipify.org)
+ipv4addr=$(curl -s https://api.ipify.org)
+curl -s "https://www.duckdns.org/update?domains=$domain&token=$token&ip=$ipv4addr&ipv6=$ipv6addr"
 EOT
-chmod +x /etc/NetworkManager/dispatcher.d/99-ddns  
+chmod +x /root/.ddns/duck.sh
+tee /etc/systemd/system/ddns.service > /dev/null <<'EOT'
+[Unit]
+Description=DynDNS Updater services
+Wants=network-online.target
+After=network-online.target
+[Service]
+Type=simple
+ExecStartPre=/bin/sleep 30
+ExecStart=/root/.ddns/duck.sh
+TimeoutSec=60
+[Install]
+WantedBy=multi-user.target
+EOT
 read -p "Enter the token from duckdns.org: " token
-sed -i "s/enter_token/$token/g" /etc/NetworkManager/dispatcher.d/99-ddns
+sed -i "s/enter_token/$token/g" /root/.ddns/duck.sh
 read -p "Enter the domain from duckdns.org: " domain
-sed -i "s/enter_domain/$domain/g" /etc/NetworkManager/dispatcher.d/99-ddns
-cat <(crontab -l) <(echo "0 */2 * * * /etc/NetworkManager/dispatcher.d/99-ddns") | crontab -
+sed -i "s/enter_domain/$domain/g" /root/.ddns/duck.sh
+systemctl enable ddns
+systemctl start ddns
+cat <(crontab -l) <(echo "0 */2 * * * /root/.ddns/duck.sh") | crontab -
 exit
