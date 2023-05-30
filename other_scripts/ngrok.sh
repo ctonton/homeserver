d6808fad07378ba86dc4f764d90ccbd8464cbdef
ctonton <51173996+ctonton@users.noreply.github.com>
2023-05-30 12:56:55 -0500

    Create ngrok.sh

---
+++ other_scripts/ngrok.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+clear
+echo "Installing ngrok."
+if [ $(dpkg --print-architecture) = "armhf" ]
+then
+  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.tgz -o ngrok.tgz
+elif [ $(dpkg --print-architecture) = "i386" ]
+then
+  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.tgz -o ngrok.tgz
+elif [ $(dpkg --print-architecture) = "arm64" ]
+then
+  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.tgz -o ngrok.tgz
+elif [ $(dpkg --print-architecture) = "amd64" ]
+then
+  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz -o ngrok.tgz
+fi
+tar xvf ngrok.tgz -C /usr/local/bin
+rm ngrok.tgz
+mkdir /root/.ngrok2
+tee /root/.ngrok2/ngrok.yml > /dev/null <<EOT
+authtoken: noauth
+tunnels:
+  nginx:
+    addr: 443
+    proto: http
+    bind_tls: true
+    inspect: false
+  ssh:
+    addr: 22
+    proto: tcp
+    inspect: false
+EOT
+tee /etc/systemd/system/ngrok.service > /dev/null <<'EOT'
+[Unit]
+Description=ngrok
+After=network-online.target
+Wants=network-online.target
+[Service]
+Type=exec
+ExecStart=/usr/local/bin/ngrok start --all
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=process
+IgnoreSIGPIPE=true
+Restart=always
+RestartSec=120
+[Install]
+WantedBy=multi-user.target
+EOT
+read -p "Enter your ngrok Authtoken: " auth
+sed -i "s/noauth/$auth/g" /root/.ngrok2/ngrok.yml
+systemctl enable ngrok
+systemctl start ngrok
+exit
