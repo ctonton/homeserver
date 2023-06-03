8d82275562faa3279b7a4821ab10993dfed1fc85
ctonton <51173996+ctonton@users.noreply.github.com>
2023-06-02 19:31:51 -0500

    Update nfs_automount.sh

--- other_scripts/nfs_automount.sh
+++ other_scripts/nfs_automount.sh
@@ -9,7 +9,7 @@ fi
 sudo tee -a /etc/auto.master > /dev/null <<EOT
 /-  /etc/auto.nfs
 EOT
-read "Enter the IP address of the NFS server: " nfsip
+read -p "Enter the IP address of the NFS server: " nfsip
 sudo tee /etc/auto.nfs > /dev/null <<EOT
 /mnt/Public  -fstype=nfs,rw,sync,soft,intr  ${nfsip}:/srv/NAS/Public
 EOT
