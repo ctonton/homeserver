796033e7bedf9805352cb1b4d06158e28cf0290f
ctonton <51173996+ctonton@users.noreply.github.com>
2023-06-19 09:25:02 -0500

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -239,6 +239,20 @@ WebUI\CSRFProtection=false
 WebUI\ClickjackingProtection=true
 WebUI\LocalHostAuth=false
 EOT
+tee /root/.config/qBittorrent/lanchk.sh > /dev/null <<'EOT'
+#!/bin/bash
+if /sbin/ip route | grep "default"
+then
+  OLD=$(cat /root/.config/qBittorrent/qBittorrent.conf | grep "AuthSubnetWhitelist=" | cut -d '=' -f 2 | cut -d '/' -f 1)
+  NEW=$(/sbin/ip route | awk '/src/ { print $1 }' | cut -d '/' -f 1)
+  if [ $OLD != $NEW ]
+  then
+    sed -i "s/$OLD/$NEW/g" /root/.config/qBittorrent/qBittorrent.conf
+  fi
+fi
+exit
+EOT
+chmod +x /root/.config/qBittorrent/lanchk.sh
 tee /etc/systemd/system/qbittorrent.service > /dev/null <<'EOT'
 [Unit]
 Description=qBittorrent Command Line Client
@@ -248,6 +262,7 @@ Type=forking
 User=root
 Group=root
 UMask=000
+ExecStartPre=/root/.config/qBittorrent/lanchk.sh
 ExecStart=/usr/bin/qbittorrent-nox -d
 [Install]
 WantedBy=multi-user.target
