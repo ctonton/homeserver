58c4ab2c553771253fa4a48bade8a0f96ac371d3
ctonton <51173996+ctonton@users.noreply.github.com>
2023-06-20 16:58:57 -0500

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -258,11 +258,14 @@ tee /root/.config/qBittorrent/lanchk.sh > /dev/null <<'EOT'
 #!/bin/bash
 if /sbin/ip route | grep "default"
 then
-  OLD=$(cat /root/.config/qBittorrent/qBittorrent.conf | grep "AuthSubnetWhitelist=" | cut -d '=' -f 2 | cut -d '/' -f 1)
+  OLD=$(cat /root/.config/route | cut -d '/' -f 1)
   NEW=$(/sbin/ip route | awk '/src/ { print $1 }' | cut -d '/' -f 1)
   if [[ $OLD != $NEW ]]
   then
     sed -i "s/$OLD/$NEW/g" /root/.config/qBittorrent/qBittorrent.conf
+    ufw delete allow from $(cat /root/.config/route)
+    ufw allow from $(/sbin/ip route | awk '/src/ { print $1 }')
+    echo $(/sbin/ip route | awk '/src/ { print $1 }') > /root/.config/route
   fi
 fi
 exit
