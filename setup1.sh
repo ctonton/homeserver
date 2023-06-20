fe84303b904d9b8e254c2ea2b70f630b51e56fa1
ctonton <51173996+ctonton@users.noreply.github.com>
2023-06-20 11:21:20 -0500

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -32,7 +32,7 @@ fi
 sed -i '0,/.*PermitRootLogin.*/s//PermitRootLogin yes/' /etc/ssh/sshd_config
 echo "0 4 * * 1 /sbin/reboot" | crontab -
 cp $0 /root/resume.sh
-sed -i '2,38d' /root/resume.sh
+sed -i '2,44d' /root/resume.sh
 chmod +x /root/resume.sh
 echo "bash /root/resume.sh" > /root/.bash_profile
 chmod +x /root/.bash_profile
