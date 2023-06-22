59237e8629a5d968556e5792dc39032dd77b6396
ctonton <51173996+ctonton@users.noreply.github.com>
2023-06-21 20:37:39 -0500

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -255,23 +255,6 @@ WebUI\CSRFProtection=false
 WebUI\ClickjackingProtection=true
 WebUI\LocalHostAuth=false
 EOT
-#tee /root/.config/qBittorrent/lanchk.sh > /dev/null <<'EOT'
-#!/bin/bash
-##if /sbin/ip route | grep "default"
-#then
-#  OLD=$(cat /root/.config/route | cut -d '/' -f 1)
-#  NEW=$(/sbin/ip route | awk '/src/ { print $1 }' | cut -d '/' -f 1)
-#  if [[ $OLD != $NEW ]]
-#  then
-#    sed -i "s/$OLD/$NEW/g" /root/.config/qBittorrent/qBittorrent.conf
-#    ufw delete allow from $(cat /root/.config/route)
-#    ufw allow from $(/sbin/ip route | awk '/src/ { print $1 }')
-#    echo $(/sbin/ip route | awk '/src/ { print $1 }') > /root/.config/route
-#  fi
-#fi
-#exit
-#EOT
-#chmod +x /root/.config/qBittorrent/lanchk.sh
 tee /etc/systemd/system/qbittorrent.service > /dev/null <<'EOT'
 [Unit]
 Description=qBittorrent Command Line Client
