b7963a0722fedf2c2f05a981bdfbe9d400d24e3e
ctonton <51173996+ctonton@users.noreply.github.com>
2023-06-22 12:32:15 -0500

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -31,7 +31,7 @@ echo
 echo "Installing software."
 apt update
 apt full-upgrade -y --fix-missing
-apt install -y --no-install-recommends firefox-esr ntfs-3g exfat-fuse tar unzip gzip ufw nfs-kernel-server samba cups printer-driver-hpcups qbittorrent-nox nginx-extras php-fpm openssl tigervnc-standalone-server novnc jwm
+apt install -y --no-install-recommends curl firefox-esr ntfs-3g exfat-fuse tar unzip gzip ufw nfs-kernel-server samba cups printer-driver-hpcups qbittorrent-nox nginx-extras php-fpm openssl tigervnc-standalone-server novnc jwm
 apt install -y --install-recommends cups-browsed openssh-server
 systemctl enable --quiet ssh
 sed -i '0,/.*PermitRootLogin.*/s//PermitRootLogin yes/' /etc/ssh/sshd_config
