4ddaca93dc823b2314be4cd34dd2c910ac837256
ctonton <51173996+ctonton@users.noreply.github.com>
2023-06-27 10:15:04 -0500

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -19,7 +19,6 @@ hostnamectl set-hostname $serv
 sed -i "s/$HOSTNAME/$serv/g" /etc/hosts
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
-echo "0 4 * * 1 /sbin/reboot" | crontab -
 
 #install
 echo
@@ -30,6 +29,7 @@ apt install -y --no-install-recommends curl ntfs-3g exfat-fuse tar unzip gzip nf
 apt install -y --install-recommends openssh-server
 systemctl enable --quiet ssh
 sed -i '0,/.*PermitRootLogin.*/s//PermitRootLogin yes/' /etc/ssh/sshd_config
+echo "0 4 * * 1 /sbin/reboot" | crontab -
 sed '2,41d' $0 > /root/resume.sh
 chmod +x /root/resume.sh
 echo "bash /root/resume.sh" > /root/.bash_profile
