91db5881bbe5b45d3a641c96da3ad8be24018205
ctonton <51173996+ctonton@users.noreply.github.com>
2023-07-09 20:46:00 -0500

    Update HC1.sh

--- other_scripts/HC1.sh
+++ other_scripts/HC1.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
+
+#board
 tee /boot/armbianEnv.txt > /dev/null <<EOT
 board_name=hc1
 usbstoragequirks=0x2537:0x1066:u,0x2537:0x1068:u
 EOT
+
+#checks
+clear
+if [[ $EUID -ne 0 ]]
+then
+  read -n 1 -s -r -p "Run as "root" user. Press any key to exit."
+  exit
+fi
+if [[ $(lsb_release -is) != "Debian" ]]
+then
+  read -n 1 -s -r -p "This script is written for the Debian OS. Press any key to exit."
+  exit
+fi
+if ! wget -q --spider www.google.com
+then
+  read -n 1 -s -r -p "The network is not online. Press any key to exit."
+  exit
+fi
+
+#initialize
+dpkg-reconfigure locales
+dpkg-reconfigure tzdata
+clear
+read -p "Enter a hostname for this server. : " serv
+hostnamectl set-hostname $serv
+sed -i "s/$HOSTNAME/$serv/g" /etc/hosts
 apt update
 systemctl --quiet disable NetworkManager
 apt autopurge -y network-manager netplan.io
@@ -10,35 +38,54 @@ rm -rf /etc/NetworkManager /etc/netplan
 apt install -y --install-recommends ifupdown
 systemctl --quiet unmask systemd-networkd
 systemctl --quiet enable systemd-networkd
+eth=$(ip route | awk '/kernel/ { print $3 }')
+mkdir -p /etc/systemd/system/systemd-networkd-wait-online.service.d
+tee /etc/systemd/system/systemd-networkd-wait-online.service.d/override.conf > /dev/null <<EOT
+[Service]
+ExecStart=
+ExecStart=/lib/systemd/systemd-networkd-wait-online --interface=$eth --timeout=30
+EOT
 tee /etc/network/interfaces > /dev/null <<EOT
 auto lo
 iface lo inet loopback
+
+auto $eth
+iface $eth inet6 auto
 EOT
-eth=$(ip route | awk '/kernel/ { print $3 }')
-read -n 1 -p "Do you want to setup a static IP address on this server? y/n: " cont
+echo
+read -p "Do you want to setup a static IP address on this server? y/n: " cont
 if [[ $cont == "y" ]]
 then
   net=$(ip route | awk '/default/ { print $3 }' | cut -d "." -f 1-3)
+  echo
   read -p "Enter a static IP address for the server: $net." add
   tee -a /etc/network/interfaces > /dev/null <<EOT
-
-auto $eth
 iface $eth inet static
         address $net.$add/24
         gateway $(ip route | awk '/default/ { print $3 }')
-        dns-nameservers 8.8.8.8 1.1.1.1
-iface $eth inet6 dhcp
 EOT
 else
-  tee -a /etc/network/interfaces > /dev/null <<EOT
-
-auto $eth
-iface $eth inet dhcp
-iface $eth inet6 dhcp
-EOT
+echo "iface $eth inet dhcp" >> /etc/network/interfaces
 fi
-wget https://raw.githubusercontent.com/ctonton/homeserver/main/setup2.sh -O setup.sh
-chmod +x setup.sh
+echo
+cont=0
+until [[ $cont == 1 ]] || [[ $cont == 2 ]]
+do
+  echo "1 - install lite version server"
+  echo "2 - install full version server"
+  read -p "Make your selection: " cont
+  if [[ $cont == 1 ]]
+  then
+    wget https://raw.githubusercontent.com/ctonton/homeserver/main/setup1.sh -O /root/setup.sh
+  elif [[ $cont == 2 ]]
+  then
+    wget https://raw.githubusercontent.com/ctonton/homeserver/main/setup2.sh -O /root/setup.sh
+  else
+    echo "Invalid selection"
+  fi
+done
+chmod +x /root/setup.sh
+sed -i '2,/#install/d' /root/setup.sh
 echo "bash /root/setup.sh" > /root/.bash_profile
 chmod +x /root/.bash_profile
 echo
