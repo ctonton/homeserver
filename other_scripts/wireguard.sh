a451e5458e09584d847403cacf1659002b2ef054
ctonton <51173996+ctonton@users.noreply.github.com>
2023-07-11 10:18:42 -0500

    Update wireguard.sh

--- other_scripts/wireguard.sh
+++ other_scripts/wireguard.sh
@@ -16,7 +16,7 @@ do
   then
     apt update
     apt install -y wireguard qrencode
-    mkdir -q /etc/wireguard
+    mkdir -p /etc/wireguard
     wg genkey | tee /etc/wireguard/private.key
     chmod go= /etc/wireguard/private.key
     cat /etc/wireguard/private.key | wg pubkey | tee /etc/wireguard/public.key
