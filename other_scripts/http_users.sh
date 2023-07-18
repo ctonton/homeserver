33034d6a46ea3b391e1224cddc1edc9f495f8d3c
ctonton <51173996+ctonton@users.noreply.github.com>
2023-07-18 11:23:49 -0500

    Update http_users.sh

--- other_scripts/http_users.sh
+++ other_scripts/http_users.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
+opt=0
 clear
-loo=0
-until [ $loo -eq 4 ]
+echo "Manage HTTP users"
+until [ $opt -eq 4 ]
 do
   echo
-  echo "1 - List users"
-  echo "2 - Add user"
-  echo "3 - Remove user"
-  echo "4 - quit"
+  echo "1 - Add user"
+  echo "2 - Remove user"
+  echo "3 - List users"
+  echo "4 - Quit"
   echo
-  read -p "Enter selection: :" loo
-  if [ $loo -eq 1 ]
-  then
-    echo
-    cat /etc/nginx/.htpasswd
-    loo=0
-  fi
-  if [ $loo -eq 2 ]
-  then
-    read -p "Enter a user name: " use
-    echo -n "${use}:" >> /etc/nginx/.htpasswd
-    openssl passwd -apr1 >> /etc/nginx/.htpasswd
-    loo=0
-  fi
-  if [ $loo -eq 3 ]
-  then
-    read -p "Enter a user name to remove: " use
-    sed -i "/$use/d" /etc/nginx/.htpasswd
-    loo=0
-  fi
-  if [ $loo -ne 0 ]
-  then
-    echo "Invalid selection."
-    echo
-  fi
+  read -p "Enter selection: " opt
+  case $opt in
+    1)
+      read -p "Enter a user name: " use
+      echo -n "${use}:" >> /etc/nginx/.htpasswd
+      openssl passwd -apr1 >> /etc/nginx/.htpasswd
+      clear
+      echo "$use added"
+      ;;
+    2)
+      PS3="Enter a number: "
+      select use in $(cat /etc/nginx/.htpasswd | cut -d ':' -f 1)
+      do
+        sed -i "/$use/d" /etc/nginx/.htpasswd
+        break
+      done
+      clear
+      echo "$use removed"
+      ;;
+    3)
+      clear
+      cat /etc/nginx/.htpasswd | cut -d ':' -f 1
+      ;;
+    *)
+      clear
+      echo "Invalid selection"
+      ;;
+  esac
 done
 exit
