314b6a695c04afc72bf537bab1fef4d6711d720c
ctonton <51173996+ctonton@users.noreply.github.com>
2023-08-06 14:02:17 -0500

    Update ngrok.sh

--- scripts/ngrok.sh
+++ scripts/ngrok.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 clear
-echo "Installing ngrok."
-if [[ $(dpkg --print-architecture) = "armhf" ]]
-then
-  wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.tgz -O ngrok.tgz
-elif [[ $(dpkg --print-architecture) = "arm64" ]]
-then
-  wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz -O ngrok.tgz
-elif [[ $(dpkg --print-architecture) = "amd64" ]]
-then
-  wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -O ngrok.tgz
-fi
-tar xvf ngrok.tgz -C /usr/local/bin
-rm ngrok.tgz
+echo "Setup ngrok."
 read -p "Enter your ngrok Authtoken: " auth
 ngrok config add-authtoken $auth
-ngrok service install --config /root/.config/ngrok/ngrok.yml
 tee -a /root/.config/ngrok/ngrok.yml > /dev/null <<EOT
 tunnels:
   nginx:
@@ -28,4 +15,5 @@ tunnels:
     addr: 22
     proto: tcp
 EOT
+ngrok service install --config /root/.config/ngrok/ngrok.yml
 exit
