7f79ff5dc256dcba209fa58e6b9b089fb047ebc3
ctonton <51173996+ctonton@users.noreply.github.com>
2023-08-16 12:30:07 -0500

    Update fixufw.sh

--- scripts/fixufw.sh
+++ scripts/fixufw.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#eth=$(ls /sys/class/net | grep e)
-eth=wlo1
-old=10.10.1.0/24
+eth=$(ls /sys/class/net | grep e)
+old=0.0.0.0/24
 new=$(ip route | grep "$eth proto kernel" | cut -d " " -f 1)
 if [ $old != $new ]; then
   ufw delete allow from $old
