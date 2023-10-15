50af5b32888b8e3a630613c3cea52a2898e0226f
ctonton <51173996+ctonton@users.noreply.github.com>
2023-10-15 15:13:00 -0500

    Update setup2.sh

--- scripts/setup2.sh
+++ scripts/setup2.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+#storage
+echo
+echo "Mounting storage."
+mkdir /srv/NAS
+chmod 777 /srv/NAS
+chown nobody:nogroup /srv/NAS
+echo
+lsblk -o NAME,TYPE,SIZE,FSTYPE,LABEL
+echo
+echo
+PS3="Select the partition to use as storage: "
+select part in $(lsblk -l -o TYPE,NAME | sed '1d' | sed '/disk/d' | cut -d " " -f 2) other
+do
+if [[ -b /dev/$part ]] && ! grep -q /dev/$part /proc/mounts
+then
+  echo "UUID=$(blkid -o value -s UUID /dev/${part})  /srv/NAS  $(blkid -o value -s TYPE /dev/${part})  defaults,nofail  0  0" >> /etc/fstab
+  mount -a
+  mkdir -p /srv/NAS/Public
+else
+  echo "Device is not available. Manually edit fstab later."
+  read -n 1 -s -r -p "Press any key to continue without mounting storage."
+fi
+break
+done
+
 #install
 echo
 echo "Installing server."
@@ -36,33 +61,6 @@ EOT
 systemctl -q enable filebrowser
 wget -q --show-progress https://github.com/ctonton/homeserver/raw/main/scripts/wireguard.sh -O /root/wireguard.sh
 chmod +x /root/wireguard.sh
-
-#storage
-echo
-echo "Mounting storage."
-mkdir /srv/NAS
-chmod 777 /srv/NAS
-chown nobody:nogroup /srv/NAS
-echo
-lsblk -o NAME,TYPE,SIZE,FSTYPE,LABEL
-echo
-echo
-PS3="Select the partition to use as storage: "
-select part in $(lsblk -l -o TYPE,NAME | sed '1d' | sed '/disk/d' | cut -d " " -f 2) other
-do
-if [[ -b /dev/$part ]] && ! grep -q /dev/$part /proc/mounts
-then
-  echo "UUID=$(blkid -o value -s UUID /dev/${part})  /srv/NAS  $(blkid -o value -s TYPE /dev/${part})  defaults,nofail  0  0" >> /etc/fstab
-  mount -a
-  mkdir -p /srv/NAS/Public
-else
-  sed -i '/^#UUID/d' /etc/fstab
-  echo "#UUID=???  /srv/NAS  ???  defaults,nofail  0  0" >> /etc/fstab
-  echo "Device is not available. Manually edit fstab later."
-  read -n 1 -s -r -p "Press any key to continue without mounting storage."
-fi
-break
-done
 tee /root/fixpermi.sh > /dev/null <<'EOT'
 #!/bin/bash
 chmod -R 777 /srv/NAS/Public
