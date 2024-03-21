13a989282286a1aa8a4313b3d3143ca993cfdf9a
ctonton <51173996+ctonton@users.noreply.github.com>
2024-03-21 10:45:06 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -27,17 +27,6 @@ else
   wget -q --show-progress https://github.com/ctonton/homeserver/raw/main/scripts/setup2.sh -O /root/setup.sh
 fi
 chmod +x /root/setup.sh
-if [[ $(ls /sys/class/net | grep ^e | wc -w) == 1 ]]
-then
-  adapt=$(ls /sys/class/net | grep ^e)
-else
-  ip route
-  PS3="Select the network adapter that this server will use to connect: "
-  select adapt in $(ls /sys/class/net | grep ^e)
-  do
-    break
-  done
-fi
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
 clear
@@ -45,25 +34,10 @@ read -p "Enter a hostname for this server. : " serv
 hostnamectl set-hostname $serv
 sed -i "s/$HOSTNAME/$serv/g" /etc/hosts
 apt update
-apt autopurge -y network-manager netplan.io ifupdown isc-dhcp-client resolvconf openvpn unattended-upgrades cloud-init firefox needrestart ufw
-rm -rf /etc/NetworkManager /etc/netplan /etc/network /etc/dhcp /var/log/unattended-upgrades /etc/cloud
-apt install -y cron networkd-dispatcher policykit-1 openssh-server systemd-resolved
+apt autopurge -y unattended-upgrades cloud-init firefox needrestart ufw
+rm -rf /var/log/unattended-upgrades /etc/cloud
+apt install -y cron openssh-server
 sed -i '0,/.*PermitRootLogin.*/s//PermitRootLogin yes/' /etc/ssh/sshd_config
-systemctl --quiet unmask systemd-networkd
-systemctl --quiet enable systemd-networkd
-mkdir -p /etc/systemd/system/systemd-networkd-wait-online.service.d
-tee /etc/systemd/system/systemd-networkd-wait-online.service.d/override.conf > /dev/null <<EOT
-[Service]
-ExecStart=
-ExecStart=/lib/systemd/systemd-networkd-wait-online --interface=$adapt --timeout=30
-EOT
-tee /etc/systemd/network/20-wired.network > /dev/null <<EOT
-[Match]
-Name=$adapt
-
-[Network]
-DHCP=yes
-EOT
 echo "bash /root/setup.sh" > /root/.bash_profile
 chmod +x /root/.bash_profile
 echo
