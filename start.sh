5b4086046d17db3d6d9e5149e2be3fb5ca29f0bc
ctonton <51173996+ctonton@users.noreply.github.com>
2024-11-19 14:01:50 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,41 +2,24 @@
 
 #checks
 clear
-if [[ $EUID -ne 0 ]]
-then
-  read -n 1 -s -r -p "Run as "root" user. Press any key to exit."
-  exit
-fi
-if ! wget -q --spider www.google.com
-then
-  read -n 1 -s -r -p "The network is not online. Press any key to exit."
-  exit
-fi
-if [[ $(lsb_release -is) != "Debian" ]]
-then
-  read -n 1 -s -r -p "This script only works with Debian Linux. Press any key to exit."
-  exit
-fi
+[[ $EUID -eq 0 ]] || (echo "Must run as root user."; exit 1)
+wget -q --spider www.google.com || (echo "The network is not online."; exit 1)
+[[ $(lsb_release -is) == "Debian" ]] || (echo "This script only works with Debian Linux."; exit 1)
 
 #initialize
-mem=$(awk '/MemTotal/ {print $2 / 1000000}' /proc/meminfo)
-if [[ ${mem%.*} -lt 1 ]]
-then
-  wget -q --show-progress https://github.com/ctonton/homeserver/raw/main/scripts/setup1.sh -O /root/setup.sh
-else
-  wget -q --show-progress https://github.com/ctonton/homeserver/raw/main/scripts/setup2.sh -O /root/setup.sh
-fi
-chmod +x /root/setup.sh
+clear
+read -p "Enter a hostname for this server. : "
+hostnamectl set-hostname $REPLY
+sed -i "s/$HOSTNAME/$REPLY/g" /etc/hosts
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
-clear
-read -p "Enter a hostname for this server. : " serv
-hostnamectl set-hostname $serv
-sed -i "s/$HOSTNAME/$serv/g" /etc/hosts
-apt update
-apt install -y cron openssh-server
+apt update && apt install -y cron openssh-server
 sed -i '0,/.*PermitRootLogin.*/s//PermitRootLogin yes/' /etc/ssh/sshd_config
-echo "bash /root/setup.sh" > /root/.bash_profile
+mem=$(awk '/MemTotal/ {print $2 / 1000000}' /proc/meminfo)
+if [[ ${mem%.*} -lt 1 ]]
+then wget -q --show-progress https://github.com/ctonton/homeserver/raw/main/scripts/setup1.sh -O /root/.bash_profile
+else wget -q --show-progress https://github.com/ctonton/homeserver/raw/main/scripts/setup2.sh -O /root/.bash_profile
+fi
 chmod +x /root/.bash_profile
 echo
 read -n 1 -s -r -p "System needs to reboot. Press any key to do so and then log in as "root" to continue."
