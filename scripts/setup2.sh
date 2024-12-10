64a98f4b7185d66eddb6e28a12c557f159784ea9
ctonton <51173996+ctonton@users.noreply.github.com>
2024-12-10 09:40:43 -0600

    Update setup2.sh

--- scripts/setup2.sh
+++ scripts/setup2.sh
@@ -98,21 +98,6 @@ cat >/etc/samba/smb.conf <<EOT
    directory mask = 0777
 EOT
 
-#minidlna
-echo
-echo "Setting up minidlna"
-[[ -f /etc/minidlna.bak ]] || mv /etc/minidlna.conf /etc/minidlna.bak
-cat >/etc/minidlna.conf <<EOT
-media_dir=V,/srv/NAS/Public/Movies
-media_dir=V,/srv/NAS/Public/Television
-db_dir=/var/cache/minidlna
-log_dir=/var/log/minidlna
-log_level=off
-port=8200
-inotify=yes
-EOT
-systemctl enable minidlna
-
 #cups
 echo
 echo "Setting up CUPS."
