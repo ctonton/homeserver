3850c484425dc230f05a83d1ecb2f77eaa9e0566
ctonton <51173996+ctonton@users.noreply.github.com>
2025-04-14 14:08:41 -0500

    Update storage.sh

--- scripts/storage.sh
+++ scripts/storage.sh
@@ -8,14 +8,10 @@ echo; lsblk -o NAME,TYPE,SIZE,FSTYPE,LABEL
 echo; echo
 PS3="Select the partition to use as storage: "
 select part in $(lsblk -l -o TYPE,NAME | sed '1d' | sed '/disk/d' | cut -d " " -f 2) other; do break; done
-if [[ -b /dev/$part ]] && ! grep -q /dev/$part /proc/mounts; then
-  sed -i "/$(blkid -o value -s UUID /dev/${part})/d" /etc/fstab
-  echo "UUID=$(blkid -o value -s UUID /dev/${part})  /srv/NAS  $(blkid -o value -s TYPE /dev/${part})  defaults,nofail  0  0" >> /etc/fstab
-  mount -a
-  mkdir -p /srv/NAS/Public
-  rm $0
-else
-  echo "Device is not available. Manually edit fstab or attach media and run this script again."
-  exit 1
-fi
+grep -q /dev/$part /proc/mounts || [[ ! -b /dev/$part ]] && (echo "Device is not available. Manually edit fstab or attach media and run this script again."; exit 1)
+sed -i "/$(blkid -o value -s UUID /dev/${part})/d" /etc/fstab
+echo "UUID=$(blkid -o value -s UUID /dev/${part})  /srv/NAS  $(blkid -o value -s TYPE /dev/${part})  defaults,nofail  0  0" >> /etc/fstab
+mount -a
+mkdir -p /srv/NAS/Public
+rm $0
 exit 0
