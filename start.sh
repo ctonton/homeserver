389d46af5da6fdb3631467958cba666734097f8d
ctonton <51173996+ctonton@users.noreply.github.com>
2025-04-14 19:05:50 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,8 +7,7 @@ wget -q --inet4-only --spider www.google.com || (echo "The network is not online
 [[ $(lsb_release -is) == "Debian" ]] || (echo "This script only works with Debian Linux."; exit 1)
 
 #initialize
-clear
-apt update && apt install -y cron curl gzip locales nano ntfs-3g openssh-server tar unzip xfsprogs
+apt update && apt install -y cron curl gzip locales nano ntfs-3g openssh-server tar unzip xfsprogs && clear
 sed -i '0,/.*PermitRootLogin.*/s//PermitRootLogin yes/' /etc/ssh/sshd_config
 read -p "Enter a hostname for this server. : "
 hostnamectl set-hostname $REPLY
@@ -16,13 +15,13 @@ sed -i "s/$HOSTNAME/$REPLY/g" /etc/hosts
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
 mem=$(awk '/MemTotal/ {print $2 / 1000000}' /proc/meminfo)
-if [[ ${mem%.*} -lt 1 ]]
-then wget -q --show-progress --inet4-only https://github.com/ctonton/homeserver/raw/main/scripts/setup1.sh -O /root/.bash_profile
-else wget -q --show-progress --inet4-only https://github.com/ctonton/homeserver/raw/main/scripts/setup2.sh -O /root/.bash_profile
+if [[ ${mem%.*} -lt 1 ]]; then
+  wget -q --show-progress --inet4-only https://github.com/ctonton/homeserver/raw/main/scripts/setup1.sh -O /root/.bash_profile
+else
+  wget -q --show-progress --inet4-only https://github.com/ctonton/homeserver/raw/main/scripts/setup2.sh -O /root/.bash_profile
 fi
 chmod +x /root/.bash_profile
-echo
-read -n 1 -s -r -p "System needs to reboot. Press any key to do so and then log in as "root" to continue."
+echo; read -n 1 -s -r -p "System needs to reboot. Press any key to do so and then log in as "root" to continue."
 rm $0
 systemctl reboot
 exit
