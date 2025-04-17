98f47f2cf9175252d3a1e97c87c9a01e780127d6
ctonton <51173996+ctonton@users.noreply.github.com>
2025-04-17 12:05:49 -0500

    Update setup1.sh

--- scripts/setup1.sh
+++ scripts/setup1.sh
@@ -20,17 +20,7 @@ fi
 mkdir -p /srv/NAS/Public
 chmod -R 777 /srv/NAS/Public
 chown -R nobody:nogroup /srv/NAS/Public
-
-#install
-apt full-upgrade -y --fix-missing
-apt install -y --no-install-recommends avahi-autoipd avahi-daemon bleachbit nfs-kernel-server nginx qbittorrent-nox rsync samba wsdd
-tee /etc/rsyncd.conf >/dev/null <<EOF
-[Public]
-  path = /srv/NAS/Public
-  comment = Public Directory
-  read only = false
-EOF
-tee /root/fixpermi.sh >/dev/null <<EOF
+tee /root/fixpermi.sh <<EOF
 #!/bin/bash
 chmod -R 777 /srv/NAS/Public
 chown -R nobody:nogroup /srv/NAS/Public
@@ -38,10 +28,22 @@ exit
 EOF
 chmod +x /root/fixpermi.sh
 
+#install
+apt full-upgrade -y --fix-missing
+apt install -y --no-install-recommends avahi-autoipd avahi-daemon bleachbit nfs-kernel-server nginx qbittorrent-nox rsync samba wsdd
+
+#rsync
+tee /etc/rsyncd.conf <<EOF
+[Public]
+  path = /srv/NAS/Public
+  comment = Public Directory
+  read only = false
+EOF
+
 #cron
-tee /root/.update.sh >/dev/null <<EOF
+tee /root/.update.sh <<EOF
 #/bin/bash
-wget -q --show-progress --inet4-only https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz -O /root/.config/qBittorrent/blocklist.p2p.gz
+wget -q --show-progress https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz -O /root/.config/qBittorrent/blocklist.p2p.gz
 gzip -df /root/.config/qBittorrent/blocklist.p2p.gz
 apt update
 apt -y upgrade
@@ -52,40 +54,10 @@ reboot
 EOF
 echo "0 4 * * 1 /root/.update.sh &>/dev/null" | crontab -
 
-#filebrowser
-tag="$(curl -s https://api.github.com/repos/filebrowser/filebrowser/releases/latest | grep 'tag_name' | cut -d '"' -f4)"
-case $(dpkg --print-architecture) in
-  armhf)
-    wget -q --show-progress --inet4-only "https://github.com/filebrowser/filebrowser/releases/download/$tag/linux-armv7-filebrowser.tar.gz" -O /root/filemanager.tar.gz;;
-  arm64)
-    wget -q --show-progress --inet4-only "https://github.com/filebrowser/filebrowser/releases/download/$tag/linux-arm64-filebrowser.tar.gz" -O /root/filemanager.tar.gz;;
-  amd64)
-    wget -q --show-progress --inet4-only "https://github.com/filebrowser/filebrowser/releases/download/$tag/linux-amd64-filebrowser.tar.gz" -O /root/filemanager.tar.gz;;
-esac
-tar -xzf /root/filemanager.tar.gz -C /usr/local/bin filebrowser
-chmod +x /usr/local/bin/filebrowser
-rm /root/filemanager.tar.gz
-wget -q --show-progress --inet4-only https://github.com/ctonton/homeserver/raw/main/files/filebrowser.zip -O /root/filebrowser.zip
-mkdir -p /root/.config
-unzip -o /root/filebrowser.zip -d /root/.config/
-rm /root/filebrowser.zip
-tee /etc/systemd/system/filebrowser.service >/dev/null <<EOF
-[Unit]
-Description=http file manager
-After=network-online.target
-Wants=network-online.target
-[Service]
-Type=exec
-ExecStart=/usr/local/bin/filebrowser -c /root/.config/filebrowser/filebrowser.json -d /root/.config/filebrowser/filebrowser.db
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl -q enable filebrowser
-
 #nfs
 [[ -f /etc/exports.bak ]] || mv /etc/exports /etc/exports.bak
 echo "/srv/NAS/Public *(rw,sync,all_squash,no_subtree_check,insecure)" > /etc/exports
-tee /etc/avahi/services/nfs.service >/dev/null <<EOF
+tee /etc/avahi/services/nfs.service <<EOF
 <?xml version="1.0" standalone='no'?>
 <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
 <service-group>
@@ -100,7 +72,7 @@ EOF
 
 #samba
 [[ -f /etc/samba/smb.bak ]] || mv /etc/samba/smb.conf /etc/samba/smb.bak
-tee /etc/samba/smb.conf >/dev/null <<EOF
+tee /etc/samba/smb.conf <<EOF
 [global]
    workgroup = WORKGROUP
    netbios name = $HOSTNAME
@@ -117,15 +89,40 @@ tee /etc/samba/smb.conf >/dev/null <<EOF
    directory mask = 0777
 EOF
 
+#filebrowser
+tag="$(curl -s https://api.github.com/repos/filebrowser/filebrowser/releases/latest | grep 'tag_name' | cut -d '"' -f4)"
+arc="$(dpkg --print-architecture)"
+[[ $arc == "armhf" ]] && arc="armv7"
+wget -q --show-progress "https://github.com/filebrowser/filebrowser/releases/download/$tag/linux-$arc-filebrowser.tar.gz" -O /root/filebrowser.tar.gz
+tar -xzf /root/filebrowser.tar.gz -C /usr/local/bin filebrowser
+chmod +x /usr/local/bin/filebrowser
+rm /root/filemanager.tar.gz
+wget -q --show-progress https://github.com/ctonton/homeserver/raw/main/files/filebrowser.zip -O /root/filebrowser.zip
+mkdir -p /root/.config
+unzip -o /root/filebrowser.zip -d /root/.config/
+rm /root/filebrowser.zip
+tee /etc/systemd/system/filebrowser.service <<EOF
+[Unit]
+Description=http file manager
+After=network-online.target
+Wants=network-online.target
+[Service]
+Type=exec
+ExecStart=/usr/local/bin/filebrowser -c /root/.config/filebrowser/filebrowser.json -d /root/.config/filebrowser/filebrowser.db
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl -q enable filebrowser
+
 #qbittorrent
 clear; echo "*** Legal Notice ***"
 echo "qBittorrent is a file sharing program. When you run a torrent, its data will be made available to others by means of upload. Any content you share is your sole responsibility."
 echo "No further notices will be issued."
 read -n 1 -s -r -p "Press any key to accept and continue..."
 mkdir -p /root/.config/qBittorrent
-wget -q --show-progress --inet4-only https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz -O /root/.config/qBittorrent/blocklist.p2p.gz
+wget -q --show-progress https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz -O /root/.config/qBittorrent/blocklist.p2p.gz
 gzip -df /root/.config/qBittorrent/blocklist.p2p.gz
-tee /root/.config/qBittorrent/qBittorrent.conf >/dev/null <<EOF
+tee /root/.config/qBittorrent/qBittorrent.conf <<EOF
 [AutoRun]
 enabled=true
 program=chown -R nobody:nogroup \"%R\"
@@ -171,13 +168,13 @@ WantedBy=multi-user.target
 EOF
 systemctl enable qbittorrent
 
-#nginx
+#html
 rm -rf /var/www/html/*
-wget -q --show-progress --inet4-only https://github.com/ctonton/homeserver/raw/main/files/icons.zip -O /root/icons.zip
+wget -q --show-progress https://github.com/ctonton/homeserver/raw/main/files/icons.zip -O /root/icons.zip
 unzip -o -q /root/icons.zip -d /var/www/html
 rm /root/icons.zip
 [[ -f /etc/nginx/sites-available/default.bak ]] || mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
-tee /var/www/html/index.html >/dev/null <<EOF
+tee /var/www/html/index.html <<EOF
 <!DOCTYPE html>
 <html>
 <head>
@@ -200,11 +197,11 @@ tee /var/www/html/index.html >/dev/null <<EOF
 EOF
 chmod -R 774 /var/www/html
 chown -R www-data:www-data /var/www/html
+
+#nginx
+[[ -f /etc/nginx/nginx.bak ]] || cp /etc/nginx/nginx.conf /etc/nginx/nginx.bak
+sed -i '/sendfile/d;/ssl_/d;s/access_log.*/access_log off\;/;s/gzip on/gzip off/' /etc/nginx/nginx.conf
 tee /etc/nginx/sites-available/default >/dev/null <<'EOF'
-map $http_upgrade $connection_upgrade {
-	default upgrade;
-	'' close;
-}
 
 upstream filebrowser {
 	server 127.0.0.1:8000;
@@ -213,7 +210,6 @@ upstream filebrowser {
 server {
 	listen 80 default_server;
 	listen [::]:80 default_server;
-	client_max_body_size 10M;
 	root /var/www/html;
 	index index.html;
 
@@ -238,6 +234,6 @@ EOF
 #cleanup
 apt -y autopurge
 read -n 1 -s -r -p "System needs to reboot. Press any key to do so."
-rm /root/.bash_profile
+rm -f /root/.bash_profile $0
 reboot
 exit 0
