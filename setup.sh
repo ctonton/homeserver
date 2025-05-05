ae7dffc727deb9bb948c070b55c4021376056c38
ctonton <51173996+ctonton@users.noreply.github.com>
2025-05-05 16:31:02 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,8 @@ mem=$(awk '/MemTotal/ {print $2 / 1000000}' /proc/meminfo) && mem=${mem%.*}
 
 #install
 systemctl -q disable unattended-upgrades --now
-apt update && apt full-upgrade -y --fix-missing
+apt update
+apt full-upgrade -y --fix-missing
 pkg=(avahi-autoipd avahi-daemon bleachbit cron curl exfat-fuse gzip locales nano nfs-kernel-server nginx ntfs-3g openssh-server qbittorrent-nox rsync samba tar tzdata unzip wsdd xfsprogs)
 [[ $mem -ge 1 ]] && pkg+=(cups-browsed cups firefox-esr jwm nginx-extras novnc openssl php-fpm printer-driver-hpcups tigervnc-standalone-server)
 apt install -y ${pkg[@]}
