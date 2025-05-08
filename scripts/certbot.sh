f717b1604defd7e51aa83651634305d5527fb0f7
ctonton <51173996+ctonton@users.noreply.github.com>
2025-05-08 10:06:55 -0500

    Update certbot.sh

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -5,6 +5,6 @@ read -p 'Enter domain without the "www." prefix: ' dom
 wom="www.$dom"
 certbot certonly --webroot -w /var/www/html -d $wom -d $dom --register-unsafely-without-email --agree-tos
 sed -i 's/ssl_certificate/#ssl_certificate/g' /etc/nginx/sites-available/default
-sed -i "/#ssl_certificate_key/a\\tssl_certificate_key \/etc\/letsencrypt\/live\/$wom\/privkey.pem\;" /etc/nginx/sites-available/default
-sed -i "/#ssl_certificate_key/a\\tssl_certificate \/etc\/letsencrypt\/live\/$wom\/fullchain.pem\;" /etc/nginx/sites-available/default
+sed -i "s/#ssl_certificate_key.*/&\n\tssl_certificate_key \/etc\/letsencrypt\/live\/$wom\/privkey.pem\;/" /etc/nginx/sites-available/default
+sed -i "s/#ssl_certificate_key.*/&\n\tssl_certificate \/etc\/letsencrypt\/live\/$wom\/fullchain.pem\;/" /etc/nginx/sites-available/default
 exit 0
