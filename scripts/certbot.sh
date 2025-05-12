7ad4257633356700f83bd4dee4115cfc71b0a369
ctonton <51173996+ctonton@users.noreply.github.com>
2025-05-11 22:37:42 -0500

    Update certbot.sh

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -7,4 +7,5 @@ certbot certonly --webroot -w /var/www/html -d $wom -d $dom --register-unsafely-
 sed -i 's/ssl_certificate/#ssl_certificate/g' /etc/nginx/sites-available/default
 sed -i "s/#ssl_certificate_key.*/&\n\tssl_certificate_key \/etc\/letsencrypt\/live\/$wom\/privkey.pem\;/" /etc/nginx/sites-available/default
 sed -i "s/#ssl_certificate_key.*/&\n\tssl_certificate \/etc\/letsencrypt\/live\/$wom\/fullchain.pem\;/" /etc/nginx/sites-available/default
+service nginx restart
 exit 0
