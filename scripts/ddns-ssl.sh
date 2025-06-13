e868e3023408d5bb792622d3edaacdf9fd79767d
ctonton <51173996+ctonton@users.noreply.github.com>
2025-06-13 16:14:01 -0500

    Update and rename certbot.sh to ddns-ssl.sh

---
+++ scripts/ddns-ssl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear ; echo "Ensure that ports 80 and 443 are open."
+echo ; read -p "Enter the token from your DuckDNS account: " tok
+echo ; read -p "Enter the domain from your DuckDNS account: " dom
+curl "https://www.duckdns.org/update?domains=${dom}&token=${tok}&ip="
+(crontab -l ; echo -e "*/15 * * * * curl \"https://www.duckdns.org/update?domains=${dom}&token=${tok}&ip=\" &> /dev/null") | crontab -
+apt update
+apt -y install certbot
+systemctl stop nginx.service
+certbot certonly --standalone -d www.${dom}.duckdns.org -d ${dom}.duckdns.org --register-unsafely-without-email --agree-tos
+echo 'pre_hook = "systemctl stop nginx.service"' >> /etc/letsencrypt/renewal/www.${dom}.duckdns.org.conf
+echo 'post_hook = "systemctl start nginx.service"' >> /etc/letsencrypt/renewal/www.${dom}.duckdns.org.conf
+sed -i 's/ssl_certificate/#ssl_certificate/g' /etc/nginx/sites-available/default
+sed -i "s/#ssl_certificate_key.*/&\n\tssl_certificate_key \/etc\/letsencrypt\/live\/www.${dom}.duckdns.org\/privkey.pem\;/" /etc/nginx/sites-available/default
+sed -i "s/#ssl_certificate_key.*/&\n\tssl_certificate \/etc\/letsencrypt\/live\/www.${dom}.duckdns.org\/fullchain.pem\;/" /etc/nginx/sites-available/default
+systemctl start nginx.service
+exit 0
