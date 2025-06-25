87262a59830d9057351aac6a0fa9c3a4168d2afb
ctonton <51173996+ctonton@users.noreply.github.com>
2025-06-25 14:16:29 -0500

    Update trixie.sh

--- trixie.sh
+++ trixie.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #name="$HOSTNAME"
-#lang="en_US.UTF-8 UTF-8" 
-#time="America/Chicago"
+#lang="en_US.UTF-8 UTF-8"
+#zone="America/Chicago"
 #part="auto"
 #acpt="true"
 
@@ -23,14 +23,14 @@ done
 systemctl -f --now disable unattended-upgrades
 apt update
 (dpkg -l locales | grep -q 'ii') || apt install -y locales
-until [ ! -z $name ] ; do
+until [ ! -z "$name" ] ; do
   echo ; echo
   read -p "Enter a hostname for this device :" name
 done
 hostnamectl set-hostname "$name"
 name=$(hostname)
 sed -i "s/$HOSTNAME/$name/g" /etc/hosts
-if [ -z $lang ] ; then
+if [ -z "$lang" ] ; then
   dpkg-reconfigure locales
 else
   grep -q -x "$lang" /usr/share/i18n/SUPPORTED || exit 1
@@ -38,11 +38,11 @@ else
   echo "$lang" > /etc/locale.gen
   dpkg-reconfigure --frontend=noninteractive locales
 fi
-if [ -z $time ] ; then
+if [ -z "$zone" ] ; then
   dpkg-reconfigure tzdata
 else
-  [ -f /usr/share/zoneinfo/"$time" ] || exit 1
-  ln -f -s /usr/share/zoneinfo/"$time" /etc/localtime
+  [ -f /usr/share/zoneinfo/"$zone" ] || exit 1
+  ln -f -s /usr/share/zoneinfo/"$zone" /etc/localtime
   dpkg-reconfigure --frontend=noninteractive tzdata
 fi
 ramm=$(awk '/MemTotal/ {print $2 / 1000000}' /proc/meminfo) && ramm=${ramm%.*}
@@ -50,7 +50,7 @@ ramm=$(awk '/MemTotal/ {print $2 / 1000000}' /proc/meminfo) && ramm=${ramm%.*}
 #install
 apt full-upgrade -y --fix-missing
 pkg=(avahi-autoipd avahi-daemon bleachbit cron curl exfat-fuse gzip locales nano nfs-kernel-server nginx ntfs-3g openssh-server qbittorrent-nox rsync samba tar tzdata unzip wsdd2 xfsprogs)
-[[ $ramm -ge 1 ]] && pkg+=(cups-browsed ffmpeg firefox-esr jwm nginx-extras nmap novnc openssl php-fpm printer-driver-hpcups shellinabox sshpass sudo tigervnc-standalone-server)
+[[ $ramm -ge 1 ]] && pkg+=(cups-browsed ffmpeg firefox-esr jwm nginx-extras nmap novnc openssl php-fpm printer-driver-hpcups shellinabox sudo tigervnc-standalone-server)
 apt install -y ${pkg[@]}
 
 #storage
@@ -283,8 +283,8 @@ chmod -R 774 /var/www/html
 chown -R www-data:www-data /var/www/html
 
 #nginx
-[ -f /etc/nginx/nginx.conf.bak ] || cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-sed -i 's/^\tssl_/\t#ssl_/;s/user www-data/user root/;s/gzip on/gzip off/;s/access_log.*/access_log off\;/' /etc/nginx/nginx.conf
+[ -f /etc/nginx/nginx.conf.bak ] && cp /etc/nginx/nginx.conf.bak /etc/nginx/nginx.conf || cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+sed -i 's/^\tssl_/\t#ssl_/;s/gzip on/gzip off/;s/access_log.*/access_log off\;/' /etc/nginx/nginx.conf
 tee /etc/nginx/sites-available/default > /dev/null << EOF
 
 upstream filebrowser {
@@ -401,45 +401,31 @@ usermod -aG lpadmin root
 defpr=$(lpstat -e | head -n 1)
 lpadmin -d $defpr
 cupsctl --no-share-printers
-mkdir -p /var/www/html/print
-tee /var/www/html/print/index.html << EOF
-<!DOCTYPE html>
-<html>
-  <body>
-    <form action="print.php" method="post" enctype="multipart/form-data">
-      <p>Select PDF to print:  <input type="file" name="fileToUpload" id="fileToUpload"></p>
-      <input type="submit" value="Upload PDF" name="submit">
-    </form>
-  </body>
-</html>
-EOF
-tee /var/www/html/print/print.php << 'EOF'
-<?php
-   if(isset($_FILES['fileToUpload'])){
-      $file_name = $_FILES['fileToUpload']['name'];
-      $file_size =$_FILES['fileToUpload']['size'];
-      $file_tmp =$_FILES['fileToUpload']['tmp_name'];
-      $file_type=$_FILES['fileToUpload']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['fileToUpload']['name'])));
-      $extensions= array("pdf","PDF");
 
-      if(in_array($file_ext,$extensions)=== false){
-         exit("File type not allowed, please choose a PDF file.");
-      }
-
-      if($file_size > 10485760){
-         exit("Maximum PDF size is 10MB, choose a smaller file.");
-      }
-
-      exec("lp $file_tmp");
-      echo "PDF sent to printer.";
-   }
-?>
+#php
+grep -q 'serv.sh' /etc/sudoers || echo 'www-data ALL=(root) NOPASSWD: /var/www/php/serv.sh' >> /etc/sudoers
+grep -q 'rout.sh' /etc/sudoers || echo 'www-data ALL=(root) NOPASSWD: /var/www/php/rout.sh' >> /etc/sudoers
+rm -rf /var/www/php
+mkdir /var/www/php
+tee /var/www/php/list.sh << 'EOF'
+#!/bin/bash
+nmap -sn -oG /dev/shm/list $(ip route | awk '/kernel/ {print $1}') > /dev/null
+sed -e '/#/d ; s/^Host\: //g ; s/).*/)\n/g' /dev/shm/list
+rm /dev/shm/list
+exit 0
 EOF
-tee /var/www/html/print/.user.ini << EOF
-upload_max_filesize = 10M
-post_max_size = 10M
+tee /var/www/php/rout.sh << 'EOF'
+#!/bin/bash
+ssh -o StrictHostKeyChecking=no root@$(ip route | awk '/default/{print$3}') 'reboot &'
+exit 0
 EOF
+tee /var/www/php/serv.sh << 'EOF'
+#!/bin/bash
+systemctl reboot
+exit 0
+EOF
+chmod -R 774 /var/www/php
+chown -R www-data:www-data /var/www/php
 
 #html
 tee /var/www/html/index.html << EOF
@@ -482,12 +468,12 @@ tee /var/www/html/index.html << EOF
       <h1>Network</h1>
       <br>
       <br>
+      <a href="/reset/"><img src="images/rst.png" alt="Reboot"></a>
+      <h1>Reboot</h1>
+      <br>
+      <br>
       <a href="/torrents/"><img src="images/qbt.png" alt="Qbittorrent"></a>
       <h1>Torrents</h1>
-      <br>
-      <br>
-      <a href="/shell/"><img src="images/tml.png" alt="Terminal"></a>
-      <h1>Terminal</h1>
       <br>
       <br>
     </div>
@@ -502,14 +488,178 @@ tee /var/www/html/index.html << EOF
       <h1>Print</h1>
       <br>
       <br>
-      <a href="/reset/"><img src="images/rst.png" alt="Reboot"></a>
-      <h1>Reboot</h1>
+      <a href="/shell/"><img src="images/tml.png" alt="Terminal"></a>
+      <h1>Terminal</h1>
       <br>
       <br>
     </div>
   </div>
 </body>
 </html>
+EOF
+mkdir -p /var/www/html/print
+tee /var/www/html/print/index.html << 'EOF'
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Print PDF</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 20px;
+    }
+    label {
+      margin-top: 10px;
+    }
+    input, select {
+      padding: 5px;
+      margin-top: 5px;
+      width: 100%;
+    }
+    button {
+      padding: 10px;
+      background-color: #4CAF50;
+      color: white;
+      border: none;
+      cursor: pointer;
+      width: 100%;
+    }
+    button:hover {
+      background-color: #45a049;
+    }
+  </style>
+</head>
+<body>
+  <h1>Print PDF on Network Printer</h1>
+  <form action="print.php" method="POST" enctype="multipart/form-data">
+    <p>
+      <label for="printer">Select Printer:</label>
+      <select name="printer" id="printer" required></select>
+    </p>
+    <p>
+      <label for="pdf">Upload PDF:</label>
+      <input type="file" name="pdf" accept=".pdf" required></input>
+    </p>
+    <h3>Print options:</h3>
+    <label for="copies">Number of Copies:</label>
+    <input type="number" name="copies" value="1" min="1"></input><br><br>
+    <label for="duplex">Duplex (Default Single-sided):</label>
+    <input type="checkbox" name="duplex" value="DuplexNoTumble"></input><br><br>
+    <label for="color">Black & White (Default Color):</label>
+    <input type="checkbox" name="color" value="Gray"></input><br><br>
+    <label for="scale">No Scaling (Default Auto):</label>
+    <input type="checkbox" name="scale" value="none"></input><br><br>
+    <button type="submit">Print PDF</button>
+  </form>
+  <script>
+    fetch('printers.php')
+    .then(response => response.json())
+    .then(data => {
+      const printerSelect = document.getElementById('printer');
+      data.printers.forEach(printer => {
+        let option = document.createElement('option');
+        option.value = printer;
+        option.textContent = printer;
+        printerSelect.appendChild(option);
+      });
+    })
+    .catch(error => console.error('Error fetching printers:', error));
+  </script>
+</body>
+</html>
+
+EOF
+tee /var/www/html/print/print.php << 'EOF'
+<?php
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $printer = $_POST['printer'];
+    $file = $_FILES['pdf']['tmp_name'];
+    $file_name = $_FILES['pdf']['name'];
+    $file_size = $_FILES['pdf']['size'];
+    $copies = isset($_POST['copies']) ? (int)$_POST['copies'] : 1;
+    $color = isset($_POST['color']) ? '-o ColorModel=Gray' : '';
+    $duplex = isset($_POST['duplex']) ? '-o Duplex=DuplexNoTumble' : '';
+    $scale = isset($_POST['scale']) ? '-o print-scaling=none' : '';
+    $ready = shell_exec("lpstat -a $printer");
+
+    if (mime_content_type($file) != 'application/pdf') {
+      echo "Error: Please upload a valid PDF file.";
+      exit;
+    }
+    if ($file_size > 10485760){
+      echo "Error: Maximum PDF size is 10MB, choose a smaller file.";
+      exit;
+    }
+
+    if (str_contains($ready, 'accepting')) {
+      exec("lp -d $printer -n $copies $color $duplex $scale $file");
+      echo "$file_name sent to $printer.";
+    }
+    else {
+      echo "Error: Selected printer is not available.";
+    }
+  }
+?>
+EOF
+tee /var/www/html/print/printers.php << 'EOF'
+<?php
+  $printers = shell_exec("lpstat -p"); 
+  $printers = explode("\n", trim($printers));
+  $printerNames = [];
+
+  foreach ($printers as $printer) {
+    if (preg_match('/^printer (\S+)/', $printer, $matches)) {
+      $printerNames[] = $matches[1];
+    }
+  }
+
+  echo json_encode(['printers' => $printerNames]);
+?>
+EOF
+tee /var/www/html/print/.user.ini << 'EOF'
+upload_max_filesize = 10M
+post_max_size = 10M
+EOF
+mkdir /var/www/html/list
+tee /var/www/html/list/index.php << 'EOF'
+<?php
+  $output = shell_exec('/bin/bash /var/www/php/list.sh');
+  echo "<pre><font size='5pt'>$output</font></pre>";
+?>
+EOF
+mkdir /var/www/html/reset
+tee /var/www/html/reset/index.html << 'EOF'
+<!DOCTYPE html>
+<html>
+<body>
+  <h1>Are you sure that you want to restart the network?</h1>
+  <form method="post" action="reboot.php">
+    <p><button style="border-radius: 8px; width: 64px;" onclick="history.back()">No</button></p>
+    <p><input style="border-radius: 8px; width: 64px;" type="submit" class="button" name="serV" value="Server"></input></p>
+    <p><input style="border-radius: 8px; width: 64px;" type="submit" class="button" name="rouT" value="Router"></input></p>
+    <p><input style="border-radius: 8px; width: 64px;" type="submit" class="button" name="botH" value="Both"></input></p>
+  </form>
+</body>
+</html>
+EOF
+tee /var/www/html/reset/reboot.php << 'EOF'
+<?php
+  if(isset($_POST["serV"])) {
+    echo "Server rebooting.";
+    exec("sudo /var/www/php/serv.sh");
+  }
+  if(isset($_POST["rouT"])) {
+    echo "Router rebooting.";
+    exec("sudo /var/www/php/rout.sh");
+  }
+  if(isset($_POST["botH"])) {
+    echo "Network rebooting.";
+    exec("sudo /var/www/php/rout.sh");
+    exec("sudo /var/www/php/serv.sh");
+  }
+?>
 EOF
 chmod -R 774 /var/www/html
 chown -R www-data:www-data /var/www/html
@@ -595,6 +745,12 @@ server {
 		fastcgi_intercept_errors on;
 		track_uploads uploads 300s;
 	}
+
+	location /print/printers.php {
+		include snippets/fastcgi-php.conf;
+		fastcgi_pass unix:/run/php/php-fpm.sock;
+	}
+
 	location /list/index.php {
 		include snippets/fastcgi-php.conf;
 		fastcgi_pass unix:/run/php/php-fpm.sock;
@@ -624,7 +780,70 @@ server {
 	}
 }
 EOF
-wget -q --show-progress https://github.com/ctonton/homeserver/raw/refs/heads/main/scripts/http_users.sh -O /root/http_users.sh
+tee /root/http_users.sh << 'EOF'
+#!/bin/bash
+clear
+echo "Manage HTTP users"
+opt=0
+until [[ $opt -eq 4 ]] ; do
+  echo
+  echo "1 - Add user"
+  echo "2 - Remove user"
+  echo "3 - List users"
+  echo "4 - Quit"
+  echo
+  read -p "Enter selection: " opt
+  case $opt in
+    1)
+      read -p "Enter a user name: " use
+      if [ -f /etc/nginx/.htpasswd ] ; then
+        echo -n "${use}:" >> /etc/nginx/.htpasswd
+        openssl passwd -apr1 >> /etc/nginx/.htpasswd
+      else
+        echo -n "${use}:" > /etc/nginx/.htpasswd
+        openssl passwd -apr1 >> /etc/nginx/.htpasswd
+        sed -i 's/#auth_basic/auth_basic/g' /etc/nginx/sites-available/default
+        systemctl restart nginx
+      fi
+      clear
+      echo "$use added"
+      ;;
+    2)
+      if [ -f /etc/nginx/.htpasswd ] ; then
+        PS3="Enter a number: "
+        select use in $(cat /etc/nginx/.htpasswd | cut -d ':' -f 1) ; do
+          sed -i "/$use/d" /etc/nginx/.htpasswd
+          break
+        done
+        if [ -z "$(cat ${file_name})" ] ; then
+          rm /etc/nginx/.htpasswd
+          sed -i 's/auth_basic/#auth_basic/g' /etc/nginx/sites-available/default
+          systemctl restart nginx
+        fi
+        clear
+        echo "$use removed"
+      else
+        clear
+        echo "No users exist."
+      fi
+      ;;
+    3)
+      if [ -f /etc/nginx/.htpasswd ]; then
+        clear
+        cat /etc/nginx/.htpasswd | cut -d ':' -f 1
+      else
+        clear
+        echo "No users exist."
+      fi
+      ;;
+    *)
+      clear
+      echo "Invalid selection"
+      ;;
+  esac
+done
+exit
+EOF
 chmod +x /root/http_users.sh
 [ -f /etc/nginx/.htpasswd ] && sed -i 's/#auth_basic/auth_basic/g' /etc/nginx/sites-available/default
 
