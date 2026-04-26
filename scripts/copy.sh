5efda4c4e66eb7ee7f9bc377fd97ff4ee483efa7
ctonton <51173996+ctonton@users.noreply.github.com>
2026-04-25 20:53:05 -0500

    Increase buffer size and block size in copy script

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -20,7 +20,7 @@ case $mode in
     done
     [[ $d == "FROM" ]] && (srce="$(lsblk -lno MOUNTPOINT /dev/"$part2")"; dest="/srv/NAS")
     [[ $d == "TO" ]] && (srce="/srv/NAS"; dest="$(lsblk -lno MOUNTPOINT /dev/"$part2")")
-    tar -b8 --directory="$srce" --exclude='Downloads' -cf - Public | mbuffer -s 4K -m 256M | tar -b8 --directory="$dest" -xf -
+    tar -b 2048 --directory="$srce" --exclude='Downloads' -cf - Public | mbuffer -s 1M -m 256M | tar -b 2048 --directory="$dest" -xf -
     mkdir -p "$dest/Public/Downloads"; chmod -R 777 "$dest/Public"; chown -R nobody:nogroup "$dest/Public"
     [[ -d /mnt/part2 ]] && (umount /mnt/part2; rmdir /mnt/part2)
   ;;
@@ -29,15 +29,15 @@ case $mode in
     ping -c 1 "$remote" &>/dev/null || (echo "Remote server unavailable."; exit 64)
     ssh -T root@"$remote" &>/dev/null <<HERE
 mbuffer --version &>/dev/null || (apt update; apt install mbuffer)
-mbuffer -s 4K -m 128M -I 7777 | tar --directory=/srv/NAS -b8 -xf - &
+mbuffer -s 1M -m 128M -I 7777 | tar --directory=/srv/NAS -b 2048 -xf - &
 HERE
-    tar --directory=/srv/NAS --exclude='Downloads' -b8 -cf - Public | mbuffer -s 4k -m 128M -O "$remote":7777
+    tar --directory=/srv/NAS --exclude='Downloads' -b 2048 -cf - Public | mbuffer -s 1M -m 128M -O "$remote":7777
     ssh root@"$remote" "mkdir -p /srv/NAS/Public/Downloads; chmod -R 777 /srv/NAS/Public; chown -R nobody:nogroup /srv/NAS/Public"
   ;;
   WAN)
     read -p "Enter a valid ip address for the remote server: " remote
     ping -c 1 "$remote" &>/dev/null || (echo "Remote server unavailable."; exit 64)
-    tar -b8 --directory=/srv/NAS --exclude='Downloads' -cf - Public | mbuffer -s 4K -m 64M | ssh root@"$remote" "tar -b8 --directory=/srv/NAS -xf -"
+    tar -b 2048 --directory=/srv/NAS --exclude='Downloads' -cf - Public | mbuffer -s 1M -m 64M | ssh root@"$remote" "tar -b 2048 --directory=/srv/NAS -xf -"
     ssh root@"$remote" "mkdir -p /srv/NAS/Public/Downloads; chmod -R 777 /srv/NAS/Public; chown -R nobody:nogroup /srv/NAS/Public"
   ;;
 esac
